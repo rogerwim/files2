de6a35b241cdb5030ea90c104a98d8aeca29bff7
Name <fjvanbommel@gmail.com>
2018-04-03 18:03:20 +0200

    msg

--- Python26/lol.py
+++ Python26/lol.py
@@ -1,4 +1,13 @@
 from Crypto import Random
+a = 1
+f = open("files.txt", 'w')
 while True:
-    IV = Random.new()
-    print(IV)
+ 	try:
+		print(a,"passes done")
+		a = a+1
+                f.write("a bit of data")
+	except:
+		file.close
+		print("we are offline now")
+		a = raw_input()
+		break
